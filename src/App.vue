3bf02023e5b1d7ef1c4ce73a0471cdcfa4cec46f
George van Vliet <george@georgevanvliet.nl>
2019-09-19 15:39:12 +0200

    Add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        Beep
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn v-if="user" text @click="signOut">Sign out</v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
 </template>
 
 <style lang="scss"></style>
