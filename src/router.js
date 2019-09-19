2491bb79e887530da406731d83d9c5c6ad45e31f
George van Vliet <george@georgevanvliet.nl>
2019-09-19 14:52:47 +0200

    Add simple 404 component

--- src/router.js
+++ src/router.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import NotFound from '@/views/NotFound'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  routes: [],
+  routes: [
+    {
+      path: '*',
+      component: NotFound,
+    },
+  ],
 })
---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>No route found for path {{ requestedRoute }}ss</div>
+</template>
+
+<script>
+export default {
+  computed: {
+    requestedRoute() {
+      return this.$route.fullPath
+    },
+  },
+}
+</script>
+
+<style scoped></style>
