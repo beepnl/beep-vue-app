647847079b1f7343d6f2fba0b347a2c32938f428
George van Vliet <george@georgevanvliet.nl>
2019-09-20 17:01:56 +0200

    Configure AWS amplify

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import Auth from '@aws-amplify/auth'
+
+Auth.configure({
+  userPoolId: process.env.VUE_APP_USER_POOL_ID,
+  userPoolWebClientId: process.env.VUE_APP_CLIENT_ID,
+  region: process.env.VUE_APP_REGION,
+})
+
+Vue.use(Auth)
 
 Vue.config.productionTip = false
 
