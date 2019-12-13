95fae49a596c29d2c05b8bc7397416aa51f52d45
Marcel van den Elst <684481+MacMaru@users.noreply.github.com>
2019-12-13 12:49:50 +0100

    add theme colors to vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify)
 
 export default new Vuetify({
   icons: {
     iconfont: 'mdi',
+  },
+  theme: {
+    themes: {
+      light: {
+        primary: colors.amber.darken2,
+        secondary: colors.teal,
+        accent: colors.deepPurple,
+      },
+    },
   },
 })
