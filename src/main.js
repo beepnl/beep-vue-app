5f2d916ac907bedb4df299505ba1a60a6e8da830
Marcel van den Elst <684481+MacMaru@users.noreply.github.com>
2020-01-14 10:21:25 +0100

    comment out unused imports

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import Auth from '@aws-amplify/auth'
 import AsyncComputed from 'vue-async-computed'
-import VueChartist from 'vue-chartist'
-import 'chartist/dist/chartist.min.css'
-import VueVirtualScroller from 'vue-virtual-scroller'
-import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
+//import VueChartist from 'vue-chartist'
+//import 'chartist/dist/chartist.min.css'
+//import VueVirtualScroller from 'vue-virtual-scroller'
+//import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
 
 import BackBar from '@/components/header/BackBar'
 Vue.component('BackBar', BackBar)
@@ -21,8 +21,8 @@ Auth.configure({
 
 Vue.use(Auth)
 Vue.use(AsyncComputed)
-Vue.use(VueChartist)
-Vue.use(VueVirtualScroller)
+//Vue.use(VueChartist)
+//Vue.use(VueVirtualScroller)
 // Vue.use(require('vue-faker'))
 
 Vue.config.productionTip = false
