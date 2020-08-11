4551bc6b6c64cafc4d108a7163f32c9ff7f46c95
JuliaBD <juliabuysingdamste@gmail.com>
2020-08-11 13:24:46 +0200

    loading view - remove menu bar + center align loading icon

--- src/router/views/_loading.vue
+++ src/router/views/_loading.vue
@@ -1,19 +1,27 @@
 <template>
-  <Layout>
+  <div class="loading">
     <Transition appear>
       <v-progress-circular size="50" color="primary" indeterminate />
     </Transition>
-  </Layout>
+  </div>
 </template>
 
 <script>
-import Layout from '@layouts/main.vue'
-
 export default {
   page: {
     title: 'Loading page...',
     meta: [{ name: 'description', content: 'Loading page...' }],
   },
-  components: { Layout },
 }
 </script>
+
+<style lang="scss" scoped>
+.loading {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  text-align: center;
+}
+</style>
