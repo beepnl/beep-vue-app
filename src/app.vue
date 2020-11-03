2c1e2afa276ba6c7df937ec73a246113d5cfdf81
JuliaBD <juliabuysingdamste@gmail.com>
2020-11-03 15:55:59 +0100

    remove npprogress spinner

--- src/app.vue
+++ src/app.vue
@@ -25,9 +25,42 @@ export default {
 <style lang="scss">
 // Style loading bar between pages.
 // https://github.com/rstacruz/nprogress
-@import '~nprogress/nprogress.css';
+
+/* Make clicks pass-through */
+#nprogress {
+  pointer-events: none;
+}
 
 #nprogress .bar {
-  background: orange;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1031;
+  width: 100%;
+  height: 2px;
+  background: $color-primary;
+}
+
+/* Fancy blur effect */
+#nprogress .peg {
+  position: absolute;
+  right: 0;
+  display: block;
+  width: 100px;
+  height: 100%;
+  box-shadow: 0 0 10px #29d, 0 0 5px #29d;
+  opacity: 1;
+  -webkit-transform: rotate(3deg) translate(0, -4px);
+  -ms-transform: rotate(3deg) translate(0, -4px);
+  transform: rotate(3deg) translate(0, -4px);
+}
+
+.nprogress-custom-parent {
+  position: relative;
+  overflow: hidden;
+}
+
+.nprogress-custom-parent #nprogress .bar {
+  position: absolute;
 }
 </style>
