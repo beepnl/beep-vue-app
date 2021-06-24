c4e9db425aacef50b384d1696d357ba187693d22
JuliaBD <juliabuysingdamste@gmail.com>
2021-06-24 09:43:47 +0200

    add finnish translations + update to 3.0.31

--- src/router/views/new.vue
+++ src/router/views/new.vue
@@ -3,8 +3,8 @@
     <v-container>
       <div class="overline mb-1 primary--text"
         ><span class="lowercase">v</span>
-        <span v-if="dutch">3.0.30 (21-06-2021) - Huidige versie</span>
-        <span v-if="!dutch">3.0.30 (21-06-2021) - Current version</span>
+        <span v-if="dutch">3.0.31 (24-06-2021) - Huidige versie</span>
+        <span v-if="!dutch">3.0.31 (24-06-2021) - Current version</span>
       </div>
       <div v-if="!dutch" class="rounded-border primary-border mb-4">
         <p class="mb-3">
