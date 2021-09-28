5124205c4f4af60984cbbc7590a3235434f2167b
JuliaBD <juliabuysingdamste@gmail.com>
2021-09-28 15:02:21 +0200

    version to 3.0.49

--- src/components/nav-drawer.vue
+++ src/components/nav-drawer.vue
@@ -82,7 +82,7 @@
 
         <div class="d-flex flex-row align-end version-number mt-n2 mr-1">
           <v-spacer></v-spacer>
-          v3.0.48
+          v3.0.49
         </div>
       </div>
     </v-navigation-drawer>
