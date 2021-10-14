4e15ff5cf56dcaa4bb6305189c08377d6ee06693
JuliaBD <juliabuysingdamste@gmail.com>
2021-10-14 13:59:16 +0200

    new - version update

--- src/router/views/new.vue
+++ src/router/views/new.vue
@@ -3,8 +3,8 @@
     <v-container>
       <div class="overline mb-1 primary--text"
         ><span class="lowercase">v</span>
-        <span v-if="dutch">3.0.66 (14-10-2021) - Huidige versie</span>
-        <span v-if="!dutch">3.0.66 (10-14-2021) - Current version</span>
+        <span v-if="dutch">3.0.67 (14-10-2021) - Huidige versie</span>
+        <span v-if="!dutch">3.0.67 (10-14-2021) - Current version</span>
       </div>
       <div v-if="!dutch" class="rounded-border primary-border mb-4">
         <p class="mb-3">
