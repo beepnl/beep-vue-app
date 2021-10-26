66573369a19200afa0648dbc3c209b3419c6ddf0
JuliaBD <juliabuysingdamste@gmail.com>
2021-10-26 10:04:50 +0200

    update translations - es + package-lock

--- src/assets/js/lang/es.js
+++ src/assets/js/lang/es.js
@@ -767,46 +767,47 @@ const translations = {
   Add_member: 'Agregar miembro',
   alert_rule_deleted: 'La regla de alerta ha sido eliminada',
 
-  /* TO BE TRANSLATED FOR v3 */
   new_or_edited_but_not_saved_sensor_defs_warning:
-    'N.B.: sensor definitions will only be saved or added after clicking the green check icon at the end of the sensor definition row in the table.',
-  delete_sensordef: 'Delete sensor definition',
-  delete_all_alerts: 'Delete all alerts',
+    'Nota: las definiciones de sensor solo serán guardadas o agregadas luego de hacer clic en el icono de verificación verde al final de la fila de definición de sensor en la tabla.',
+  delete_sensordef: 'Eliminar la definición del sensor',
+  delete_all_alerts: 'Eliminar todas las alertas',
   delete_all_alerts_warning:
-    'Are you sure you want to delete all alerts? This cannot be undone.',
+    '¿Está seguro que desea borrar todas las alertas? Esto no se puede deshacer.',
   delete_all_alerts_warning_filter_active:
-    'Are you sure you want to delete all alerts? Alerts that do not match your search term will be deleted as well. This cannot be undone.',
-  already_verified: 'I have verified my email address and would like to login',
-  password_recovery_resend_mail: 'Send new verification code',
-  alert_rule_created: 'New alert rule has been created',
-  select_all: 'Select all',
-  During: 'During', // context: 'During 2 days', or 'During 1 hour', to notify the period during which the same alert has been triggered (each evaluation) in a row
+    '¿Está seguro que desea eliminar todas las alertas? También se eliminarán las alertas que no coincidan con su término de búsqueda. Esto no se puede deshacer.',
+  already_verified:
+    'Yo he verificado mi dirección de correo electrónico y me gustaría iniciar sesión',
+  password_recovery_resend_mail: 'Enviar nuevo código de verificación',
+  alert_rule_created: 'Nueba regla de alerta ha sido creada',
+  select_all: 'Seleccionar todos',
+  During: 'Durante',
   Calculation_minutes:
-    'How often would you like the calculation to be performed?',
-  Every: 'Every ', // context: how often would you like the alert rule calculation to be performed? -> every 15 min, ..., every 24 hours etc.
+    '¿Con qué frecuencia le gustaría que el cálculo fuera realizado?',
+  Every: 'Cada ',
   every: 'cada ',
-  Calculation_minutes_short: 'How often is it calculated?', // for alert rule overview table header
-  Alert_on_occurences_short: 'When do you receive the alert?', // for alert rule overview table header
-  Disable_alert_for_this_hive: 'Disable alert for this hive',
-  Alert_disabled_for_this_hive: 'Alert is disabled for this hive',
-  disabled_for_hive: 'has been disabled for hive', // [alert] has been disabled for the following hive
-  Alert_disabled: 'Alert disabled',
+  Calculation_minutes_short: '¿Con qué frecuencia es calculada?',
+  Alert_on_occurences_short: '¿Cuándo recibes la alerta?',
+  Disable_alert_for_this_hive: 'Deshabilitar alerta para esta colmena',
+  Alert_disabled_for_this_hive: 'Alerta está deshabilitada para esta colmena',
+  disabled_for_hive: 'ha sido deshabilitada para la colmena',
+  Alert_disabled: 'Alerta deshabilitada',
 
-  // For soon to be launched new log data import page
-  Log_data_import: 'Import log data',
-  Log_files: 'Log files',
-  Upload_date: 'Upload date',
-  Messages: 'Messages',
-  Log_time: 'Time logged',
-  File_size: 'File size',
-  check_log_data: 'Check log data',
-  delete_log_file: 'Delete log file',
-  commit_log_data_short: 'Import log data',
-  commit_log_data: 'Import data from this log file to the BEEP app: ',
+  /* for import page */
+  Log_data_import: 'Importar datos de registro',
+  Log_files: 'Archivos de registro',
+  Upload_date: 'Actualizar fecha',
+  Messages: 'Mensajes',
+  Log_time: 'Tiempo registrado',
+  File_size: 'Tamaño del archivo',
+  check_log_data: 'Chequear datos de registro',
+  delete_log_file: 'Eliminar archivo de registro',
+  commit_log_data_short: 'Importar datos de registro',
+  commit_log_data:
+    'Importar datos desde este archivo de registro hacia la aplicación BEEP',
   import_log_data_explanation:
-    "In the BEEP base app you can download log data from the internal memory of the BEEP base. Every time you download log data, the internal memory gets cleared and the data gets uploaded to the BEEP app. Below is the list of your downloaded log data. You can replace the data in the database by the data inside the downloaded log files by clicking the button 'Check log data'. This will first check how much data will be replaced inside the database, and if the time of the data in the log file is correct. You will get a confirmation box with the resulting info, and the possibility to apply or deny the replacement.",
+    'En la aplicación de la base BEEP Usted puede descargar datos de registro desde la memoria interna de la base BEEP. Cada vez que Usted descargue datos de registro, la memoria interna es desocupada and los datos son cargados a la aplicación BEEP. Abajo está la lista de su registro de datos descargados. Usted puede reemplazar los datos en la base de datos dentro de los archivos de datos descargados haciendo click en el botón ´Chequear datos de registro`. Esto chequeará cuantos datos serán reeemplazados al interior de la base de datos, y si el tiempo de los datos en archivo registro es correcto. Usted recibirá una carpeta de confirmación con la información resultante, y la posibilidad de aplicar o negar el reemplazo.',
   import_log_data_url_text:
-    'Here you can find the support article on downloading log data with the BEEP base app.',
+    'Aquí Usted puede encontrar el artículo de apoyo sobre descargar datos de registro con la aplicación de la base BEEP.',
   import_log_data_support_url:
     'https://beepsupport.freshdesk.com/es/support/solutions/articles/60000697129-descargar-datos-de-base-de-beep-a-trav%C3%A9s-de-bluetooth',
 
