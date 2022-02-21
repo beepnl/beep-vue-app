c5dd86160fd1fd5412a9f7a217bdefba99b3a764
JuliaBD <juliabuysingdamste@gmail.com>
2022-02-21 11:04:15 +0100

    flashlog - start with loading by default + version to 3.0.85

--- src/router/views/flashlog.vue
+++ src/router/views/flashlog.vue
@@ -244,7 +244,7 @@ export default {
   mixins: [momentFormat, readTaxonomy, sensorMixin],
   data() {
     return {
-      loading: false,
+      loading: true,
       noChartData: false,
       blockData: null,
       errorMessage: null,
