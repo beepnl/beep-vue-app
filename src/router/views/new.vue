2a4cf3b9d4fd55d483d757e0539e2aa37a20f838
JuliaBD <juliabuysingdamste@gmail.com>
2022-08-01 11:40:49 +0200

    new - update current version date

--- src/router/views/new.vue
+++ src/router/views/new.vue
@@ -2,7 +2,7 @@
   <Layout :title="$t('Whats_new')">
     <v-container>
       <div class="overline mb-1"
-        ><span class="lowercase">v</span>3.0.101 (14-07-2022) - Current
+        ><span class="lowercase">v</span>3.0.101 (01-08-2022) - Current
         version</div
       >
       <div class="rounded-border mb-4">
