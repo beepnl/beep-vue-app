5e532e7c5ead288053880023841cfbc8cf4d3e7d
JuliaBD <juliabuysingdamste@gmail.com>
2022-08-23 07:50:18 +0200

    version to 3.0.102

--- src/router/views/new.vue
+++ src/router/views/new.vue
@@ -2,8 +2,17 @@
   <Layout :title="$t('Whats_new')">
     <v-container>
       <div class="overline mb-1"
-        ><span class="lowercase">v</span>3.0.101 (01-08-2022) - Current
+        ><span class="lowercase">v</span>3.0.102 (23-08-2022) - Current
         version</div
+      >
+      <div class="rounded-border mb-4">
+        <ul>
+          <li>Bugfixes: small fixes for issues received as feedback</li>
+        </ul>
+      </div>
+
+      <div class="overline mb-1"
+        ><span class="lowercase">v</span>3.0.101 (01-08-2022)</div
       >
       <div class="rounded-border mb-4">
         <ul>
