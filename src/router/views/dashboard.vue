4f3069a9fec439bbfe2a6843a390eef10f1ecdf7
JuliaBD <juliabuysingdamste@gmail.com>
2023-04-13 16:24:25 +0200

    version to 1.2

--- src/router/views/dashboard.vue
+++ src/router/views/dashboard.vue
@@ -7,7 +7,7 @@
       <span
         v-if="showControls"
         class="font-small color-grey"
-        v-text="'v1.1'"
+        v-text="'v1.2'"
       ></span>
       <v-spacer />
       <div class="d-flex justify-end">
