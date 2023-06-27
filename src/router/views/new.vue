8cef5f8aa991180b6097b4baa6c73a64415bbda3
JuliaBD <juliabuysingdamste@gmail.com>
2023-06-27 09:58:45 +0200

    new - updated text

--- src/router/views/new.vue
+++ src/router/views/new.vue
@@ -2,7 +2,7 @@
   <Layout :title="$t('Whats_new')">
     <v-container>
       <div class="overline mb-1"
-        ><span class="lowercase">v</span>4.0.9 (19-06-2023) - Current
+        ><span class="lowercase">v</span>4.0.9 (27-06-2023) - Current
         version</div
       >
       <div class="rounded-border mb-4">
