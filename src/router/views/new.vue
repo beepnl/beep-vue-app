2653d523a7f2e5ba7facafb219895ab1382d9e24
JuliaBD <juliabuysingdamste@gmail.com>
2023-08-31 16:44:34 +0200

    new - update text + version to 4.0.15

--- src/router/views/new.vue
+++ src/router/views/new.vue
@@ -2,8 +2,17 @@
   <Layout :title="$t('Whats_new')">
     <v-container>
       <div class="overline mb-1"
-        ><span class="lowercase">v</span>4.0.14 (31-08-2023) - Current
+        ><span class="lowercase">v</span>4.0.15 (31-08-2023) - Current
         version</div
+      >
+      <div class="rounded-border mb-4">
+        <ul>
+          <li>Decision support</li>
+        </ul>
+      </div>
+
+      <div class="overline mb-1"
+        ><span class="lowercase">v</span>4.0.14 (31-08-2023)</div
       >
       <div class="rounded-border mb-4">
         <ul>
