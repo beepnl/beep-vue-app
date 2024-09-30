f559a73e6e78659e20f9a5596a7b66eba56ae8ac
JuliaBD <juliabuysingdamste@gmail.com>
2024-09-30 12:11:01 +0200

    version to 4.0.21 + update package-lock-only to lockfile-version 1 + new - update text

--- src/router/views/new.vue
+++ src/router/views/new.vue
@@ -2,8 +2,17 @@
   <Layout :title="$t('Whats_new')">
     <v-container>
       <div class="overline mb-1"
-        ><span class="lowercase">v</span>4.0.20 (12-08-2024) - Current
+        ><span class="lowercase">v</span>4.0.21 (30-09-2024) - Current
         version</div
+      >
+      <div class="rounded-border mb-4">
+        <ul>
+          <li>Small improvements</li>
+        </ul>
+      </div>
+
+      <div class="overline mb-1"
+        ><span class="lowercase">v</span>4.0.20 (12-08-2024)</div
       >
       <div class="rounded-border mb-4">
         <ul>
