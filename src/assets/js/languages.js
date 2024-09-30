cbe5f9525e4c92f0893d213c8e5c26780208cdca
JuliaBD <juliabuysingdamste@gmail.com>
2024-09-30 11:45:27 +0200

    locale-changer - prevent userlocale reset if database value is not yet available

--- src/assets/js/languages.js
+++ src/assets/js/languages.js
@@ -21,28 +21,24 @@ const languages = {
     // { lang: 'en-US', title: 'English US' },
   ],
   checkBrowserLanguage() {
-    var matched = null
-    var languagePartials = navigator.language.split('-')[0]
-    this.languageArray.map((language) => {
-      if (language.lang === navigator.language) {
-        matched = language.lang
-      }
-    })
-    if (!matched) {
+    let matched = 'en' // fallback
+    const navLanguage = navigator.language
+    if (navLanguage) {
+      const languagePartials = navLanguage.split('-')[0]
       this.languageArray.map((language) => {
-        if (language.lang === languagePartials) {
+        if (language.lang === navigator.language) {
+          matched = language.lang
+        } else if (language.lang === languagePartials) {
+          matched = language.lang
+        } else if (language.lang.split('-')[0] === languagePartials) {
           matched = language.lang
         }
+        return matched
       })
+      return matched
+    } else {
+      return matched // fallback
     }
-    if (!matched) {
-      this.languageArray.map((language) => {
-        if (language.lang.split('-')[0] === languagePartials) {
-          matched = language.lang
-        }
-      })
-    }
-    return matched
   },
 }
 
