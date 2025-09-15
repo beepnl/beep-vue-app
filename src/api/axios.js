aa75335daa9abf83fafe8bd5548a62ef769a193e
JuliaBD <juliabuysingdamste@gmail.com>
2025-09-15 10:12:41 +0200

    upgrade axios to 1.12.0 + fix axios script accordingly

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,17 +1,26 @@
 import router from '@router'
-import axios from 'axios'
 import store from '@state/store'
+import axios from 'axios'
 
 const instance = axios.create({
   baseURL:
     process.env.VUE_APP_API_URL || process.env.VUE_APP_BASE_API_URL_FALLBACK,
 })
 
+// Alter defaults after instance has been created
+instance.defaults.headers.common['Content-Type'] = 'application/json'
+
 // On 401 error, reset user and redirect to login
 const UNAUTHORIZED = 401
 instance.interceptors.response.use(
-  (response) => response,
-  (error) => {
+  function onFulfilled(response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response
+  },
+  function onRejected(error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
     console.log(error)
     const status = error.response ? error.response.status : 'No response'
     const originalRequest = error.config
@@ -27,23 +36,30 @@ instance.interceptors.response.use(
       originalRequest._retry = true
       return axios(originalRequest)
     }
+
     return Promise.reject(error)
   }
 )
 
 // Dynamically add API token to requests
 // Dynamically add Accept-Language to requests
-instance.interceptors.request.use(function(config) {
-  config.headers.common['Content-Type'] = 'application/json'
-  const apiToken = store.getters['auth/apiToken']
-  if (apiToken) {
-    config.headers.common.Authorization = 'Bearer ' + apiToken
+instance.interceptors.request.use(
+  function(config) {
+    // config.headers.common['Content-Type'] = 'application/json'
+    const apiToken = store.getters['auth/apiToken']
+    if (apiToken) {
+      instance.defaults.headers.common.Authorization = 'Bearer ' + apiToken
+    }
+    const locale = store.getters['auth/userLocale']
+    if (locale) {
+      instance.defaults.headers.common['Accept-Language'] = locale
+    }
+    return config
+  },
+  function(error) {
+    // Do something with request error
+    return Promise.reject(error)
   }
-  const locale = store.getters['auth/userLocale']
-  if (locale) {
-    config.headers.common['Accept-Language'] = locale
-  }
-  return config
-})
+)
 
 export default instance
