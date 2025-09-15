eadd46c03b1f1e5025759118c220f525d6155cef
JuliaBD <juliabuysingdamste@gmail.com>
2025-09-15 12:24:13 +0200

    upgrade axios to 1.12.0 + fix axios script accordingly + version to 4.0.28 + package-lock version 1

--- src/router/views/new.vue
+++ src/router/views/new.vue
@@ -2,8 +2,17 @@
   <Layout :title="$t('Whats_new')">
     <v-container>
       <div class="overline mb-1"
-        ><span class="lowercase">v</span>4.0.27 (31-03-2025) - Current
+        ><span class="lowercase">v</span>4.0.28 (15-09-2025) - Current
         version</div
+      >
+      <div class="rounded-border mb-4">
+        <ul>
+          <li>Bugfixes: small fixes for issues received as feedback</li>
+        </ul>
+      </div>
+
+      <div class="overline mb-1"
+        ><span class="lowercase">v</span>4.0.27 (31-03-2025)</div
       >
       <div class="rounded-border mb-4">
         <ul>
