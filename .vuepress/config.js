b95e7b100b2a9922dace4aaf8606dd4b70657397
JuliaBD <juliabuysingdamste@gmail.com>
2025-10-30 14:05:03 +0100

    vue-3 - remove vuepress because of critical issues and not compatible with Vue 3

--- .vuepress/config.js
+++
@@ -1,9 +0,0 @@
-const appConfig = require('../public/manifest')
-
-module.exports = {
-  title: appConfig.title + ' Docs',
-  description: appConfig.description,
-  themeConfig: {
-    sidebar: [['/']],
-  },
-}
