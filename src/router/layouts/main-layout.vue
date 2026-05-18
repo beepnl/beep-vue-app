452757c94b8bdc6ce654664b7911f4f53b564717
JuliaBD <juliabuysingdamste@gmail.com>
2026-05-18 14:31:26 +0200

    main-layout - fix underline correct active tab

--- src/router/layouts/main-layout.vue
+++ src/router/layouts/main-layout.vue
@@ -40,6 +40,7 @@
 
     <template v-slot:extension>
       <v-tabs
+        :model-value="activeTab.id"
         stacked
         density="compact"
         grow
@@ -51,6 +52,7 @@
         <v-tab
           v-for="(tab, i) in tabs"
           :key="i"
+          :model-value="tab.id"
           :to="{ name: tab.route }"
           :exact="tab.exact"
           @click="reloadData(tab.route)"
@@ -114,121 +116,142 @@
 </template>
 
 <script>
-import LocaleChanger from '@components/locale-changer.vue'
-import NavDrawer from '@components/nav-drawer.vue'
-import PlusMenu from '@components/plus-menu.vue'
+import LocaleChanger from "@components/locale-changer.vue";
+import NavDrawer from "@components/nav-drawer.vue";
+import PlusMenu from "@components/plus-menu.vue";
 import {
   checkAlerts,
   nativeAppMethods,
   readApiariesAndGroups,
   readDevicesIfNotChecked,
-  readGeneralInspections,
-} from '@mixins/methodsMixin'
-import { PWAPrompt } from 'vue-ios-pwa-prompt'
-import { mapGetters } from 'vuex'
+  readGeneralInspections
+} from "@mixins/methodsMixin";
+import { PWAPrompt } from "vue-ios-pwa-prompt";
+import { mapGetters } from "vuex";
 
 export default {
   components: {
     LocaleChanger,
     NavDrawer,
     PlusMenu,
-    PWAPrompt,
+    PWAPrompt
   },
   mixins: [
     checkAlerts,
     nativeAppMethods,
     readApiariesAndGroups,
     readDevicesIfNotChecked,
-    readGeneralInspections,
+    readGeneralInspections
   ],
   data: function() {
     return {
       drawer: false,
       assetsUrl:
-        import.meta.env.VITE_ASSETS_URL ||
-        import.meta.env.VITE_ETS_URL_FALLBACK,
-    }
+        import.meta.env.VITE_ASSETS_URL || import.meta.env.VITE_ETS_URL_FALLBACK
+    };
   },
   computed: {
-    ...mapGetters('alerts', ['alerts']),
-    ...mapGetters('devices', ['devices']),
-    currentRoute() {
-      return this.$route.name
+    ...mapGetters("alerts", ["alerts"]),
+    ...mapGetters("devices", ["devices"]),
+    activeTab() {
+      const activeTabs = this.tabs.filter(tab => tab.active);
+      return activeTabs.length > 0 ? activeTabs[0] : this.tabs[0];
     },
-    tabs: function() {
+    currentRoute() {
+      return this.$route.name;
+    },
+    tabs() {
       if (this.devices.length > 0) {
         return [
           {
-            title: this.$i18n.tc('Hive_short', 2),
-            icon: 'mdi-home-analytics',
-            route: 'home',
+            title: this.$i18n.tc("Hive_short", 2),
+            icon: "mdi-home-analytics",
+            route: "home",
             exact: true,
+            active: this.currentRoute === "home",
+            id: 0
           },
           {
-            title: this.$i18n.tc('Inspection', 2),
-            icon: 'mdi-file-document-edit-outline',
-            route: 'diary',
+            title: this.$i18n.tc("Inspection", 2),
+            icon: "mdi-file-document-edit-outline",
+            route: "diary",
             exact: false,
+            active: this.currentRoute === "diary",
+            id: 1
           },
           {
-            title: this.$i18n.t('data'),
-            icon: 'mdi-chart-line',
-            route: 'measurements',
+            title: this.$i18n.t("data"),
+            icon: "mdi-chart-line",
+            route: "measurements",
             exact: false,
+            active:
+              this.currentRoute === "measurements" ||
+              this.currentRoute === "measurements-id",
+            id: 2
           },
           {
-            title: this.$i18n.tc('Alert', 2),
-            icon: 'mdi-bell',
-            route: 'alerts',
+            title: this.$i18n.tc("Alert", 2),
+            icon: "mdi-bell",
+            route: "alerts",
             exact: false,
-          },
-        ]
+            active: this.currentRoute === "alerts",
+            id: 3
+          }
+        ];
       } else {
         return [
           {
-            title: this.$i18n.tc('Hive_short', 2),
-            icon: 'mdi-home-analytics',
-            route: 'home',
+            title: this.$i18n.tc("Hive_short", 2),
+            icon: "mdi-home-analytics",
+            route: "home",
             exact: true,
+            active: this.currentRoute === "home",
+            id: 0
           },
           {
-            title: this.$i18n.tc('Inspection', 2),
-            icon: 'mdi-file-document-edit-outline',
-            route: 'diary',
+            title: this.$i18n.tc("Inspection", 2),
+            icon: "mdi-file-document-edit-outline",
+            route: "diary",
             exact: false,
+            active: this.currentRoute === "diary",
+            id: 1
           },
           {
-            title: this.$i18n.t('data'),
-            icon: 'mdi-chart-line',
-            route: 'measurements',
+            title: this.$i18n.t("data"),
+            icon: "mdi-chart-line",
+            route: "measurements",
             exact: false,
-          },
-        ]
+            active:
+              this.currentRoute === "measurements" ||
+              this.currentRoute === "measurements-id",
+            id: 2
+          }
+        ];
       }
-    },
+    }
   },
   created() {
-    this.readDevicesIfNotChecked()
+    this.readDevicesIfNotChecked();
   },
   methods: {
     reloadData(route) {
-      if (route === 'alerts') {
-        this.checkAlertRulesAndAlerts()
-      } else if (route === 'diary') {
-        this.readGeneralInspections()
-      } else if (route === 'home') {
-        this.checkAlertRulesAndAlerts()
-        this.readApiariesAndGroups()
+      if (route === "alerts") {
+        this.checkAlertRulesAndAlerts();
+      } else if (route === "diary") {
+        this.readGeneralInspections();
+      } else if (route === "home") {
+        this.checkAlertRulesAndAlerts();
+        this.readApiariesAndGroups();
       }
     },
     clearHiveFilters() {
-      this.$store.commit('locations/clearFilters')
+      this.$store.commit("locations/clearFilters");
     },
     updateRoute(val) {
-      this.$router.push(val) // respond to tab swipes
-    },
-  },
-}
+      this.$router.push(val); // respond to tab swipes
+    }
+  }
+};
 </script>
 
 <style lang="scss">
