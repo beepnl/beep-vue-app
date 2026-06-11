7863cd57e4707aed0b47786ec4ce7b2bc51f086d
JuliaBD <juliabuysingdamste@gmail.com>
2026-06-11 14:51:35 +0200

    reload via router on vue module load error

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router'
 // https://github.com/declandewet/vue-meta
 // import VueMeta from 'vue-meta'
 // Adds a loading bar at the top during page loads.
 // import NProgress from 'nprogress/nprogress'
-import store from "@state/store";
-import routes from "./routes";
+import store from '@state/store'
+import routes from './routes'
 
 const router = createRouter({
   routes,
@@ -18,12 +18,21 @@ const router = createRouter({
   // https://router.vuejs.org/guide/advanced/scroll-behavior.html
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
-      return savedPosition;
+      return savedPosition
     } else {
-      return { x: 0, y: 0 };
+      return { x: 0, y: 0 }
     }
+  },
+})
+
+router.onError((error, to) => {
+  if (
+    error.message.includes('Failed to fetch dynamically imported module') ||
+    error.message.includes('Importing a module script failed')
+  ) {
+    window.location = to.fullPath
   }
-});
+})
 
 // Before each route evaluates...
 router.beforeEach((routeTo, routeFrom) => {
@@ -35,37 +44,37 @@ router.beforeEach((routeTo, routeFrom) => {
 
   // Check if auth is required on this route
   // (including nested routes).
-  const authRequired = routeTo.matched.some(route => route.meta.authRequired);
+  const authRequired = routeTo.matched.some((route) => route.meta.authRequired)
 
   // If auth isn't required for the route, just continue.
-  if (!authRequired) return true;
+  if (!authRequired) return true
 
-  if (store.getters["auth/loggedIn"]) {
+  if (store.getters['auth/loggedIn']) {
     // If the user is logged in, continue
-    return true;
+    return true
   } else if (
-    store.getters["auth/apiToken"] &&
-    !store.getters["auth/loggedIn"]
+    store.getters['auth/apiToken'] &&
+    !store.getters['auth/loggedIn']
   ) {
     // If the user is empty but api token is present in local storage (f.e. after hard refresh), first validate user.
-    store.dispatch("auth/validateUser").then(() => {
-      return true;
-    });
+    store.dispatch('auth/validateUser').then(() => {
+      return true
+    })
   } else {
     // If the user is NOT currently logged in and no api token is present redirect to login.
-    redirectToLogin();
+    redirectToLogin()
   }
 
   function redirectToLogin() {
     // Pass the original route to the login component
     // Pass the original route to the login component
-    const query = { redirectFrom: routeTo.fullPath };
+    const query = { redirectFrom: routeTo.fullPath }
     //  and add queries separately (if present) so they keep working (as is needed for the language query)
-    Object.assign(query, routeTo.query);
+    Object.assign(query, routeTo.query)
 
-    return { path: "sign-in", query };
+    return { path: 'sign-in', query }
   }
-});
+})
 
 router.beforeResolve(async (routeTo, routeFrom) => {
   // Create a `beforeResolve` hook, which fires whenever
@@ -91,26 +100,26 @@ router.beforeResolve(async (routeTo, routeFrom) => {
               // }
               // Complete the redirect.
               // return { ...args[0] }
-              reject(new Error("Redirected"));
+              reject(new Error('Redirected'))
             } else {
-              resolve();
+              resolve()
             }
-          });
+          })
         } else {
           // Otherwise, continue resolving the route.
-          resolve();
+          resolve()
         }
-      });
+      })
     }
     // If a `beforeResolve` hook chose to redirect, just return.
   } catch (error) {
-    console.log("Vue-router beforeResolve error", error);
-    return false;
+    console.log('Vue-router beforeResolve error', error)
+    return false
   }
 
   // If we reach this point, continue resolving the route.
-  return true;
-});
+  return true
+})
 
 // When each route is finished evaluating...
 // router.afterEach((routeTo, routeFrom) => {
@@ -120,17 +129,17 @@ router.beforeResolve(async (routeTo, routeFrom) => {
 
 // When each route is finished evaluating store previous route name
 router.afterEach((routeTo, routeFrom) => {
-  localStorage.beepPreviousRoute = routeFrom.name;
-  localStorage.beepPreviousQueryHiveIndex = routeFrom.query.hive_index;
+  localStorage.beepPreviousRoute = routeFrom.name
+  localStorage.beepPreviousQueryHiveIndex = routeFrom.query.hive_index
   // if navigating away from home / one of the other home tabs, remember it in order to return when back button is hit in a page with depth 1
   if (routeFrom.meta.depth === 0) {
     if (routeFrom.meta.tabWithIDParamRequired === true) {
       // when the tab route has an id param (like measurements, where these meta props are specified), save the fallback as beepPreviousTab instead
-      localStorage.beepPreviousTab = routeFrom.meta.tabFallback;
+      localStorage.beepPreviousTab = routeFrom.meta.tabFallback
     } else {
-      localStorage.beepPreviousTab = routeFrom.name;
+      localStorage.beepPreviousTab = routeFrom.name
     }
   }
-});
+})
 
-export default router;
+export default router
